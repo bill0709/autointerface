202d4d363f839f1eca80b3894abb54064882499a
weihaitao <weihaitao0510@163.com>
2017-11-10 21:23:50 +0800

    really update hhh

--- runtest2report.py
+++ runtest2report.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     runner = HTMLTestRunner(stream=fp, title=u'自动化接口测试报告', description=u'以下用例执行情况:')
     runner.run(discover)
     fp.close()
-    print 'gggg'
-    print 'gggg'
+    print 'hhh'
+    print 'hhh'
+

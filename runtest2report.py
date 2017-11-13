64f1b60cebd72bf066dd11bcd42eabda8a41a181
weihaitao <weihaitao0510@163.com>
2017-11-13 15:12:33 +0800

    old person update

--- runtest2report.py
+++ runtest2report.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     runner = HTMLTestRunner(stream=fp, title=u'自动化接口测试报告', description=u'以下用例执行情况:')
     runner.run(discover)
     fp.close()
-    print 'modify first'
+    print ' new person modify'
+    print 'i am new person join pro'
     print 'old person update'
 

91dd6c707cecd7cd8e3450818df8e46143c66443
weihaitao <weihaitao0510@163.com>
2017-11-14 14:58:56 +0800

    buyaozhushi

--- runtest2report.py
+++ runtest2report.py
@@ -14,11 +14,4 @@ if __name__ == '__main__':
     runner = HTMLTestRunner(stream=fp, title=u'自动化接口测试报告', description=u'以下用例执行情况:')
     runner.run(discover)
     fp.close()
-    print 'modify first'
-    print 'old person update'
-    print 'old person update everyday branch'
-    print '********'
-    print '********'
-    print '********'
-    print '********'
 
